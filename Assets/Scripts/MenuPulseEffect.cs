5ffbf0576df63e3c7ed5d6ddccc243563ef70241
EQKim <114055097+EQKim@users.noreply.github.com>
2026-04-29 16:59:52 +0200

    UI Update (Part 1-3)
    Main Menu Buttons

--- Assets/Scripts/MenuPulseEffect.cs
+++ Assets/Scripts/MenuPulseEffect.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using TMPro;
 
 public class MenuPulseEffect : MonoBehaviour, ISelectHandler, IDeselectHandler
 {
@@ -8,20 +7,12 @@ public class MenuPulseEffect : MonoBehaviour, ISelectHandler, IDeselectHandler
     [SerializeField] private float pulseSpeed = 4f;
     [SerializeField] private float pulseAmount = 0.08f;
 
-    [Header("Colors")]
-    [SerializeField] private bool changeColor = true;
-    [SerializeField] private Color normalColor = Color.white;
-    [SerializeField] private Color selectedColor = Color.yellow;
-
     private Vector3 baseScale;
     private bool isSelected;
-    private TMP_Text tmpText;
 
     private void Awake()
     {
         baseScale = transform.localScale;
-        tmpText = GetComponentInChildren<TMP_Text>();
-
         ResetVisuals();
     }
 
@@ -34,16 +25,17 @@ public class MenuPulseEffect : MonoBehaviour, ISelectHandler, IDeselectHandler
         }
         else
         {
-            transform.localScale = Vector3.Lerp(transform.localScale, baseScale, Time.unscaledDeltaTime * 10f);
+            transform.localScale = Vector3.Lerp(
+                transform.localScale,
+                baseScale,
+                Time.unscaledDeltaTime * 10f
+            );
         }
     }
 
     public void OnSelect(BaseEventData eventData)
     {
         isSelected = true;
-
-        if (tmpText != null && changeColor)
-            tmpText.color = selectedColor;
     }
 
     public void OnDeselect(BaseEventData eventData)
@@ -60,8 +52,5 @@ public class MenuPulseEffect : MonoBehaviour, ISelectHandler, IDeselectHandler
     {
         isSelected = false;
         transform.localScale = baseScale;
-
-        if (tmpText != null && changeColor)
-            tmpText.color = normalColor;
     }
 }
